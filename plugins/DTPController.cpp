6552a5e2259140621744080579bea49c2e067319
Jim B <jim.brooke@cern.ch>
2022-07-15 00:17:25 +0100

    Jbrooke/masking (#14)
    * adopt refactored methods in dtpcontrols
    
    * put link setup back in
    
    * complete config function
    
    * add channel masks to config
    
    * add params for threshold and mask commands
    
    * backout masks until this is working
    
    * put masks back in
    
    * print masks in hex

--- plugins/DTPController.cpp
+++ plugins/DTPController.cpp
@@ -108,45 +108,80 @@ namespace dunedaq {
       
       // here we will need to setup the FW config
       // ie. number of links, number of pipes etc.
-
+      int n_links = m_dtp_pod->get_n_links();
+      int n_streams = m_dtp_pod->get_n_streams();
 
       // reset
+      TLOG_DEBUG(TLVL_INFO) << get_name() << ": resetting DTP pod";
       m_dtp_pod->reset();
       
       // set CRIF to drop empty
+      TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting CRIF to drop empy packets";
       m_dtp_pod->get_crif_node().set_drop_empty();
+
+      // enable CRIF
+      TLOG_DEBUG(TLVL_INFO) << get_name() << ": enabling CRIF output";
+      m_dtp_pod->get_crif_node().enable();
 
       // set source
       if (m_dtp_cfg.source == "ext") {
+	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting input to GBT";
 	m_dtp_pod->get_flowmaster_node().set_source_gbt();
       }
       else {
+	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting input to wibulator";
 	m_dtp_pod->get_flowmaster_node().set_source_wtor();
+	for (int i=0; i < n_links; ++i) {
+	  m_dtp_pod->get_wibulator_node(i).load_WIB_pattern_from_file(m_dtp_cfg.pattern);
+	}
       }
 
       // set sink
+      TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting sink to hits";
       m_dtp_pod->get_flowmaster_node().set_sink_hits();
 
-      // setup links
-      int n_links = m_dtp_pod->get_n_links();
+      // set high threshold - initially
+      TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting TP threshold to maximum (0x7fff)";
+      for (int i_link = 0; i_link<n_links; ++i_link) {
+	for (int i_stream = 0; i_stream<n_streams; ++i_stream) {
+	  m_dtp_pod->get_link_processor_node(i_link).set_threshold(i_stream, 0x7fff);
+	}
+      }
+
+      // enable links
       for (int i = 0; i<n_links; ++i) {
 	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting up link processor " << i;
-	m_dtp_pod->get_link_processor_node(i).setup(true, true, 0x7fff);
+	m_dtp_pod->get_link_processor_node(i).setup(true, true);
 	sleep(1);
       }
 
-      // set masks - need to decide the interface/data structure
-      //      m_dtp_pod->set_channel_mask(m_dtp_cfg.mask);
+      // set masks
+      int masks_size = 20; // m_dtp_cfg.masks.size();
+      if (masks_size == n_links * n_streams) {
 
-      // reudce threshold for running
+	for (int i_link=0; i_link < n_links; i_link++) {
+	  for (int i_stream=0; i_stream < n_streams; i_stream++) {
+	    
+	    int i_mask = (i_link * n_streams) + i_stream;
+	    uint64_t mask = m_dtp_cfg.masks.at(i_mask);
+	    TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting masks for link " << i_link << " stream " << i_stream << " to " << std::hex << mask;
+	    m_dtp_pod->get_link_processor_node(i_link).set_channel_mask_all( i_stream, mask );
+	    
+	  }
+	}
+
+      }
+
+
+      // reduce threshold for running
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": waiting for pedestal to settle...";
 
       sleep(2);
-      for (int i = 0; i<n_links; ++i) {
-	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting threshold for link processor " << i;
-	for (int j = 0; j<m_dtp_pod->get_n_streams(); ++j) {
-	  m_dtp_pod->get_link_processor_node(i).get_stream_proc_array_node().stream_select(j, true);
-	  m_dtp_pod->get_link_processor_node(i).get_stream_proc_array_node().get_stream_proc_node().set_threshold(m_dtp_cfg.threshold, true);
+
+      for (int i_link = 0; i_link<n_links; ++i_link) {
+	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting threshold for link processor " << i_link << " to " << m_dtp_cfg.threshold;
+	for (int i_stream = 0; i_stream<n_streams; ++i_stream) {
+	  m_dtp_pod->get_link_processor_node(i_link).set_threshold(i_stream, m_dtp_cfg.threshold);
 	}
       }
 
@@ -159,26 +194,24 @@ namespace dunedaq {
 
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": Entering do_start() method";
 
-      if (m_dtp_pod) {
-	m_dtp_pod->enable_crif();
+      if (m_dtp_cfg.source == "int") {
+	if (m_dtp_pod) {
+	  for (int i=0; i < m_dtp_pod->get_n_links(); ++i) {
+	    m_dtp_pod->get_wibulator_node(i).fire();
+	  }
+	}
+	else {
+	  throw ModuleNotConfigured(ERS_HERE, std::string("DTPController"));
+	}
       }
-      else {
-        throw ModuleNotConfigured(ERS_HERE, std::string("DTPController"));
-      }
-
+      
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": Exiting do_start() method";
     }
 
     void
     DTPController::do_stop(const data_t& /* args */) {
-      TLOG_DEBUG(TLVL_INFO) << get_name() << ": Entering do_stop() method";
 
-      if (m_dtp_pod) {
-	m_dtp_pod->disable_crif();
-      }
-      else {
-        throw ModuleNotConfigured(ERS_HERE, std::string("DTPController"));
-      }
+      TLOG_DEBUG(TLVL_INFO) << get_name() << ": Entering do_stop() method";
 
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": Exiting do_stop() method";
     }
--- schema/dtpctrllibs/dtpcontroller.jsonnet
+++ schema/dtpctrllibs/dtpcontroller.jsonnet
@@ -18,21 +18,29 @@ local cs = {
     uint_data: s.number("UintData", "u4",
         doc="A count of very many things"),
 
+    arr_uint_data: s.sequence("ArrUintData", cs.uint_data,
+        doc="An array of values"),
+
     double_data: s.number("DoubleData", "f8", 
          doc="A double"),
-      
+
     conf: s.record("ConfParams",[
         s.field("connections_file", self.str, "",
             doc="uHAL connections file"),
         s.field("device", self.str, "",
-            doc="string name of managed device"),
+            doc="String name of managed device"),
 	s.field("uhal_log_level", self.uhal_log_level, "notice",
-            doc="log level for uhal. Possible values are: fatal, error, warning, notice, info, debug."),
+            doc="Log level for uhal. Possible values are: fatal, error, warning, notice, info, debug."),
         s.field("source", self.str, "ext",
 	    doc="Data source (int or ext)"),
+        s.field("pattern", self.str, "",
+            doc="Path to pattern file (data source = int)"),
 	s.field("threshold", self.uint_data, "20",
             doc="Trigger primitive threshold"),
+	s.field("masks", self.arr_uint_data, [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            doc="Trigger primitive threshold"),
     ], doc="Structure for payload of DTP configure commands"),
+
 };
 
 s_app + moo.oschema.sort_select(cs)
