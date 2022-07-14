b5caf7f36c4ea1993e058cf74a4ca728870b931d
Jim <jim.brooke@cern.ch>
2022-07-14 23:35:40 +0100

    put masks back in

--- plugins/DTPController.cpp
+++ plugins/DTPController.cpp
@@ -111,10 +111,9 @@ namespace dunedaq {
       int n_links = m_dtp_pod->get_n_links();
       int n_streams = m_dtp_pod->get_n_streams();
 
-
       // reset
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": resetting DTP pod";
-       m_dtp_pod->reset();
+      m_dtp_pod->reset();
       
       // set CRIF to drop empty
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting CRIF to drop empy packets";
@@ -132,16 +131,19 @@ namespace dunedaq {
       else {
 	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting input to wibulator";
 	m_dtp_pod->get_flowmaster_node().set_source_wtor();
+	for (int i=0; i < n_links; ++i) {
+	  m_dtp_pod->get_wibulator_node(i).load_WIB_pattern_from_file(m_dtp_cfg.pattern);
+	}
       }
 
       // set sink
-	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting sink to hits";
+      TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting sink to hits";
       m_dtp_pod->get_flowmaster_node().set_sink_hits();
 
       // set high threshold - initially
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting TP threshold to maximum (0x7fff)";
-      for (uint32_t i_link = 0; i_link<static_cast<uint32_t>(n_links); ++i_link) {
-	for (uint32_t i_stream = 0; i_stream<static_cast<uint32_t>(m_dtp_pod->get_n_streams()); ++i_stream) {
+      for (int i_link = 0; i_link<n_links; ++i_link) {
+	for (int i_stream = 0; i_stream<n_streams; ++i_stream) {
 	  m_dtp_pod->get_link_processor_node(i_link).set_threshold(i_stream, 0x7fff);
 	}
       }
@@ -157,12 +159,11 @@ namespace dunedaq {
       int masks_size = 20; // m_dtp_cfg.masks.size();
       if (masks_size == n_links * n_streams) {
 
-	for (uint32_t i_link=0; i_link < static_cast<uint32_t>(n_links); i_link++) {
-	  for (uint32_t i_stream=0; i_stream < static_cast<uint32_t>(n_streams); i_stream++) {
+	for (int i_link=0; i_link < n_links; i_link++) {
+	  for (int i_stream=0; i_stream < n_streams; i_stream++) {
 	    
 	    int i_mask = (i_link * n_streams) + i_stream;
-	    //	    uint64_t mask = m_dtp_cfg.masks.at(i_mask);
-	    uint64_t mask = 0;
+	    uint64_t mask = m_dtp_cfg.masks.at(i_mask);
 	    TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting masks for link " << i_link << " stream " << i_stream << " to " << mask;
 	    m_dtp_pod->get_link_processor_node(i_link).set_channel_mask_all( i_stream, mask );
 	    
@@ -172,14 +173,14 @@ namespace dunedaq {
       }
 
 
-      // reudce threshold for running
+      // reduce threshold for running
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": waiting for pedestal to settle...";
 
       sleep(2);
 
-      for (uint32_t i_link = 0; i_link<static_cast<uint32_t>(n_links); ++i_link) {
+      for (int i_link = 0; i_link<n_links; ++i_link) {
 	TLOG_DEBUG(TLVL_INFO) << get_name() << ": setting threshold for link processor " << i_link << " to " << m_dtp_cfg.threshold;
-	for (uint32_t i_stream = 0; i_stream<static_cast<uint32_t>(m_dtp_pod->get_n_streams()); ++i_stream) {
+	for (int i_stream = 0; i_stream<n_streams; ++i_stream) {
 	  m_dtp_pod->get_link_processor_node(i_link).set_threshold(i_stream, m_dtp_cfg.threshold);
 	}
       }
@@ -190,29 +191,25 @@ namespace dunedaq {
     
     void
     DTPController::do_start(const data_t& /* args */) {
-
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": Entering do_start() method";
 
-      if (m_dtp_pod) {
-	m_dtp_pod->enable_crif();
+      if (m_dtp_cfg.source == "int") {
+	if (m_dtp_pod) {
+	  for (int i=0; i < m_dtp_pod->get_n_links(); ++i) {
+	    m_dtp_pod->get_wibulator_node(i).fire();
+	  }
+	}
+	else {
+	  throw ModuleNotConfigured(ERS_HERE, std::string("DTPController"));
+	}
       }
-      else {
-        throw ModuleNotConfigured(ERS_HERE, std::string("DTPController"));
-      }
-
+      
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": Exiting do_start() method";
     }
 
     void
     DTPController::do_stop(const data_t& /* args */) {
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": Entering do_stop() method";
-
-      if (m_dtp_pod) {
-	m_dtp_pod->disable_crif();
-      }
-      else {
-        throw ModuleNotConfigured(ERS_HERE, std::string("DTPController"));
-      }
 
       TLOG_DEBUG(TLVL_INFO) << get_name() << ": Exiting do_stop() method";
     }
